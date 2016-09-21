93426ae5628685c0d3e6afe2c10a28d11666ffbd
Ian Delairre <idelairre@gmail.com>
2016-09-20 22:42:36 -0400

    program now outputs addresses starting at 7A060, modifies tests collection so it iterates in order, modifies test to match against new output

--- disassembler.java
+++ disassembler.java
@@ -1,26 +1,27 @@
 import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Collections;
 import java.math.BigInteger;
 
 abstract class Procedure {
-  protected String address;
+  protected static int address = 0x7A05C;
 
-  public Procedure(int addr) {
-    address = Integer.toHexString(addr);
+  public Procedure() {
+    address = address + 4;
   }
 
   public static String getRegister(int reg) {
     return new String('$' + Integer.toString(reg));
   }
 
-  public String getAddress() {
+  public int getAddress() {
     return address;
   }
 
   abstract public void print();
-  
+
   abstract public String toString();
 
 }
@@ -30,7 +31,6 @@ class RFormat extends Procedure {
   private String rt;
   private String rd;
   private String funct;
-  private String address;
 
   private static final Map<Integer, String> functCodes;
 
@@ -44,40 +44,42 @@ class RFormat extends Procedure {
     functCodes = Collections.unmodifiableMap(tempMap);
   }
 
-  public RFormat(int addr) {
-    super(addr);
-    rs = setRs(addr);
-    rt = setRt(addr);
-    rd = setRd(addr);
-    funct = setFunct(addr);
+  public RFormat(int hex) {
+    super();
+    rs = setRs(hex);
+    rt = setRt(hex);
+    rd = setRd(hex);
+    funct = setFunct(hex);
   }
 
-  private static String setRs(int address) {
-    int reg = (address & 0x3E00000) >>> 21;
+  private static String setRs(int hex) {
+    int reg = (hex & 0x3E00000) >>> 21;
     return getRegister(reg);
   }
 
-  private static String setRt(int address) {
-    int reg = (address & 0x1F0000) >>> 16;
+  private static String setRt(int hex) {
+    int reg = (hex & 0x1F0000) >>> 16;
     return getRegister(reg);
   }
 
-  private static String setRd(int address) {
-    int reg = (address & 0xF800) >>> 11;
+  private static String setRd(int hex) {
+    int reg = (hex & 0xF800) >>> 11;
     return getRegister(reg);
   }
 
-  private static String setFunct(int address) {
-    int functCode = (address & 0x3F);
+  private static String setFunct(int hex) {
+    int functCode = (hex & 0x3F);
     return functCodes.get(functCode);
   }
 
   public String toString() {
-    return new String(funct + " " + rd + ", " + rs + ", " + rt);
+    String currentAddress = Integer.toHexString(address);
+    String procedure = funct + " " + rd + ", " + rs + ", " + rt;
+    return currentAddress + " " + procedure;
   }
 
   public void print() {
-    System.out.println(getAddress() + ": " + toString());
+    System.out.println(toString());
   }
 }
 
@@ -100,55 +102,56 @@ class IFormat extends Procedure {
     opCodes = Collections.unmodifiableMap(tempMap);
   }
 
-  public IFormat(int addr) {
-    super(addr);
-    opcode = setOpcode(addr);
-    rs = setRs(addr);
-    rd = setRd(addr);
-    offset = setOffset(addr);
+  public IFormat(int hex) {
+    super();
+    opcode = setOpcode(hex);
+    rs = setRs(hex);
+    rd = setRd(hex);
+    offset = setOffset(hex);
 
   }
 
-  private static String setRd(int address) {
-    int reg = (address & 0x1F0000) >>> 16;
+  private static String setRd(int hex) {
+    int reg = (hex & 0x1F0000) >>> 16;
     return getRegister(reg);
   }
 
-  private static String setRs(int address) {
-    int reg = (address & 0x3E00000) >>> 21;
+  private static String setRs(int hex) {
+    int reg = (hex & 0x3E00000) >>> 21;
     return getRegister(reg);
   }
 
-  public int getBranch(int address, short offset) {
+  public int getBranch(short offset) {
     return address + offset;
   }
 
-  private String setOffset(int address) {
-    short offset = (short) (address & 0xFFFF);
+  private String setOffset(int hex) {
+    short offset = (short) (hex & 0xFFFF);
     if (opcode.equals("beq") || opcode.equals("bne")) {
-      int branch = getBranch(address, offset);
-      return new String(Integer.toHexString(branch)).toUpperCase();
+      int branch = getBranch(offset);
+      return new String(Integer.toHexString(branch));
     }
     return String.valueOf(offset);
   }
 
-  static String setOpcode(int address) {
-    int opcode = (address & 0xFC000000) >>> 26;
+  static String setOpcode(int hex) {
+    int opcode = (hex & 0xFC000000) >>> 26;
     return opCodes.get(opcode);
   }
 
   public String toString() {
     String procedure;
+    String currentAddress = Integer.toHexString(address);
     if (opcode.equals("beq") || opcode.equals("bne")) {
-      procedure = new String(opcode + " " + rs + ", " + rd + ", address " + offset);
+      procedure = opcode + " " + rs + ", " + rd + ", address " + offset;
     } else {
-      procedure = new String(opcode + " " + rd + ", " + offset + '(' + rs + ')');
+      procedure = opcode + " " + rd + ", " + offset + '(' + rs + ')';
     }
-    return procedure;
+    return currentAddress + " " + procedure;
   }
 
-  public void print () {
-    System.out.println(address + " " + toString());
+  public void print() {
+    System.out.println(toString());
   }
 
 }
@@ -160,24 +163,23 @@ class Disassembler {
   public static final Map<Integer, String> tests;
 
   static {
-    Map<Integer, String> tempMap = new HashMap<Integer, String>();
+    Map<Integer, String> tempMap = new LinkedHashMap<Integer, String>(); // need a linked hash map so these iterate in order
     // R-Format
-    tempMap.put(0x01398820, "add $17, $9, $25");
-    tempMap.put(0x01398824, "and $17, $9, $25");
-    tempMap.put(0x01398822, "sub $17, $9, $25");
-    tempMap.put(0x01398825, "or $17, $9, $25");
-    tempMap.put(0x0139882a, "slt $17, $9, $25");
+    tempMap.put(0x01398820, "7a060 add $17, $9, $25");
+    tempMap.put(0x01398824, "7a064 and $17, $9, $25");
+    tempMap.put(0x01398822, "7a068 sub $17, $9, $25");
+    tempMap.put(0x01398825, "7a06c or $17, $9, $25");
+    tempMap.put(0x0139882a, "7a070 slt $17, $9, $25");
     // I-Format
-    tempMap.put(0x8d3104d2, "lw $17, 1234($9)");
-    tempMap.put(0x8d31fb2e, "lw $17, -1234($9)");
-    tempMap.put(0xad3104d2, "sw $17, 1234($9)");
-    tempMap.put(0xad31fb2e, "sw $17, -1234($9)");
-    tempMap.put(0x1229ffff, "beq $17, $9, address 1229FFFE"); // -1
-    tempMap.put(0x1229000e, "beq $17, $9, address 1229001C"); // 14
-    tempMap.put(0x1629fff1, "bne $17, $9, address 1629FFE2"); // -15
-    tempMap.put(0x16290000, "bne $17, $9, address 16290000"); // 0
-    tempMap.put(0xAE77FFFC, "sw $23, -4($19)");
-
+    tempMap.put(0x8d3104d2, "7a074 lw $17, 1234($9)");
+    tempMap.put(0x8d31fb2e, "7a078 lw $17, -1234($9)");
+    tempMap.put(0xad3104d2, "7a07c sw $17, 1234($9)");
+    tempMap.put(0xad31fb2e, "7a080 sw $17, -1234($9)");
+    tempMap.put(0x1229ffff, "7a084 beq $17, $9, address 7a083"); // -1
+    tempMap.put(0x1229000e, "7a088 beq $17, $9, address 7a096"); // 14
+    tempMap.put(0x1629fff1, "7a08c bne $17, $9, address 7a07d"); // -15
+    tempMap.put(0x16290000, "7a090 bne $17, $9, address 7a090"); // 0
+    tempMap.put(0xAE77FFFC, "7a094 sw $23, -4($19)");
     tests = Collections.unmodifiableMap(tempMap);
   }
 
@@ -224,7 +226,7 @@ class Disassembler {
   }
 
   public static void main(String[] args) {
-    parseAddresses();
-    // test();
+    // parseAddresses();
+    test();
   }
 }
